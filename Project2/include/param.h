be4c04d429d4788a84bdcaafd75775d05f7815ed
KimJangHyeon <rlawkdgus75@gmail.com>
2018-09-29 16:57:36 +0900

    setting: type, param

---
+++ Project2/include/param.h
@@ -0,0 +1,22 @@
+
+
+//============= NUMBER ================
+//init
+#define INIT_PAGE_NUM	8
+
+//constant
+#define LRECORD			4//31
+#define IRECORD			4//248
+
+
+//============= OFFSET ================
+//init
+//constant
+
+
+
+//============= SIZE ==================
+#define PAGESIZE		4096
+#define PAGEHSIZE		128
+#define PAGETHSIZE		64
+
---
+++ Project2/include/types.h
@@ -0,0 +1,7 @@
+#include <inttypes.h>
+
+typedef uint64_t uoffset_t;
+typedef uint64_t unumber_t;
+typedef uint64_t utable_t;
+typedef uint64_t ukey64_t;
+typedef uint32_t ukey32_t;
